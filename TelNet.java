80f7313e3e3c0a1f105745f194fc5df21285eb09
Stephan Trube <sttrube@htwg-konstanz.de>
2016-01-26 02:52:24 +0100

    using hashmap for IDs fixed random generation to satisfy specification reworked graphics to work on various screen and grid resolutions

--- TelNet.java
+++ TelNet.java
@@ -1,21 +1,21 @@
 package Telefonnetz;
 
+import javax.swing.*;
 import java.awt.*;
-import java.util.LinkedList;
+import java.util.*;
 import java.util.List;
-import java.util.PriorityQueue;
 
 public class TelNet {
 
     int lbg;
-    List<TelKnoten> telKnoten;
+    Map<TelKnoten, Integer> telKnoten;
     List<TelVerbindung> telVerbindungen;
     List<TelVerbindung> minSpanTree;
 
     // Legt ein neues Telefonnetz mit dem Leitungsbegrenzungswert lbg an.
     public TelNet(int lbg) {
         this.lbg = lbg;
-        telKnoten = new LinkedList<>();
+        telKnoten = new HashMap<>();
         telVerbindungen = new LinkedList<>();
         minSpanTree = new LinkedList<>();
     }
@@ -23,11 +23,11 @@ public class TelNet {
     // Fügt einen neuen Telefonknoten mit Koordinate (x,y) dazu.
     public boolean addTelKnoten(int x, int y) {
         TelKnoten tk = new TelKnoten(x, y);
-        if (telKnoten.contains(tk)) return false;
+        if (telKnoten.containsKey(tk)) return false;
 
-        telKnoten.add(tk);
+        telKnoten.put(tk, telKnoten.size());    // .size() entspricht dem zu vergebenden Index
 
-        for (TelKnoten tk2 : telKnoten) {
+        for (TelKnoten tk2 : telKnoten.keySet()) {
             if (tk.equals(tk2)) continue;
             int dist = distanz(tk, tk2);
             if (dist < lbg) {
@@ -51,19 +51,15 @@ public class TelNet {
 
         while (forest.size() != 1 && !edges.isEmpty()) {
             TelVerbindung telVer = edges.poll();
+
             TelKnoten source = telVer.getSource();
             TelKnoten target = telVer.getTarget();
-            int counter = 0;
-            int one = 0;
-            int two = 0;
-            for (TelKnoten tk : telKnoten) {
-                if (source.equals(tk)) one = counter;
-                if (target.equals(tk)) two = counter;
-                counter++;
-            }
 
-            int t1 = forest.find(one);
-            int t2 = forest.find(two);
+            int sourceID = telKnoten.get(source);
+            int targetID = telKnoten.get(target);
+
+            int t1 = forest.find(sourceID);
+            int t2 = forest.find(targetID);
 
             if (t1 != t2) {
                 forest.union(t1, t2);
@@ -74,6 +70,7 @@ public class TelNet {
 		if (edges.isEmpty() && forest.size() != 1) {
         	return false;
         } else {
+            minSpanTree.clear();
             minSpanTree.addAll(spanTree);
             return true;
         }
@@ -83,31 +80,62 @@ public class TelNet {
     public void	drawOptTelNet(int xMax, int yMax) throws IllegalStateException {
         checkSuccess();
 
-        StdDraw.clear();
-        StdDraw.setCanvasSize(xMax, yMax);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // holt Bildschirmauflösung
+        double width = screenSize.getWidth() - 5;
+        double height = screenSize.getHeight() - 88;    // 88 scheint die kombinierte Höhe von Titel- und Menüleiste zu sein
+        float aspect = (float)width / (float)height;
+
+        StdDraw.setCanvasSize((int)width, (int)height);     // beinahe Vollbild
+        StdDraw.setXscale(0, aspect);
+
+        float factorX = 1f / xMax * aspect;
+        float factorY = 1f / yMax;
+
+        float radius = Math.min(factorX, factorY) * 0.45f;  // benachbarte Kreise würden überlappen falls > 0.5f
+
         for(TelVerbindung telVer : getOptTelNet()) {
-            int x1 = telVer.getSource().x;
-            int y1 = telVer.getSource().y;
-            int x2 = telVer.getTarget().x;
-            int y2 = telVer.getTarget().y;
+            float x1 = telVer.getSource().x * factorX;
+            float y1 = telVer.getSource().y * factorY;
+            float x2 = telVer.getTarget().x * factorX;
+            float y2 = telVer.getTarget().y * factorY;
 
             StdDraw.setPenColor(Color.BLUE);
-            StdDraw.filledCircle(x1, y1, 3);
-            StdDraw.filledCircle(x2, y2, 3);
+            StdDraw.filledCircle(x1, y1, radius);
+            StdDraw.filledCircle(x2, y2, radius);
 
             StdDraw.setPenColor(Color.GRAY);
             StdDraw.line(x1, y1, x2, y1);
             StdDraw.line(x2, y1, x2, y2);
         }
-        StdDraw.show();
     }
 
     //Fügt n zufällige Telefonknoten zum Netz dazu mit x-Koordinate aus [0,xMax] und y-Koordinate aus [0,yMax].
     public void	generateRandomTelNet(int n, int xMax, int yMax) {
+
+        int maxDurchlauf = 100000;              // wie oft soll ein neuer Ort gesucht werden, falls kein freier gefunden wurde?
         for (int i = 0; i < n; i++) {
-            int x = (int)(xMax * Math.random());
-            int y = (int)(yMax * Math.random());
-            addTelKnoten(x, y);
+
+            int x, y;
+            boolean bereitsVorhanden = false;
+            int durchlauf = 0;
+            do {                                                // diese while-Schleife garantiert "n" Knoten statt "bis zu n"
+                x = (int) (xMax * Math.random());
+                y = (int) (yMax * Math.random());
+
+                for (TelKnoten tk : telKnoten.keySet()) {        // haben wir an dieser Stelle bereits einen TelKnoten?
+                    if (tk.x == x && tk.y == y) {
+                        bereitsVorhanden = true;
+                        break;
+                    }
+                }
+            } while (bereitsVorhanden && durchlauf++ < maxDurchlauf);
+
+            if (bereitsVorhanden) {
+                System.out.println("Selbst nach " + maxDurchlauf + " Versuchen konnte kein freier Platz für Knoten " + telKnoten.size() + " gefunden werden.");
+                System.exit(-maxDurchlauf);
+            } else {
+                addTelKnoten(x, y);
+            }
         }
     }
 
@@ -135,10 +163,16 @@ public class TelNet {
     }
 
     public static void	main(String[] args) {
-        TelNet net = new TelNet(1000);
-        net.generateRandomTelNet(500, 500, 500);
+
+        int lbg = 150;
+        int xMax = 300;
+        int yMax = 200;//xMax;
+        int nodes = 200;
+
+        TelNet net = new TelNet(lbg);
+        net.generateRandomTelNet(nodes, xMax, yMax);
         net.computeOptTelNet();
-        net.drawOptTelNet(500, 500);
+        net.drawOptTelNet(xMax, yMax);
     }
 
     // Liefert die Anzahl der Knoten des Telefonnetzes zurück.
